97725b826d3469a4ace8d2cf952b2e8844cd1173
jisus <jisus123427@gmail.com>
2024-01-26 22:45:43 +0500

    Добавил подсчёт очков в класс  Striker

--- main.py
+++ main.py
@@ -27,6 +27,30 @@ class Striker:
         self.geekRect = pygame.Rect(posx, posy, width, height)
         self.geek = pygame.draw.rect(screen, self.color, self.geekRect)
 
+    def display(self):
+        self.geek = pygame.draw.rect(screen, self.color, self.geekRect)
+
+    def update(self, yline):
+        self.posy = self.posy + self.speed * yline
+
+        if self.posy <= 0:
+            self.posy = 0
+
+        elif self.posy + self.height >= HEIGHT:
+            self.posy = HEIGHT - self.height
+
+        self.geekRect = (self.posx, self.posy, self.width, self.height)
+
+    def displayScore(self, text, score, x, y, color):
+        text = font20.render(text + str(score), True, color)
+        textRect = text.get_rect()
+        textRect.center = (x, y)
+
+        screen.blit(text, textRect)
+
+    def getRect(self):
+        return self.geekRect
+
 
 class Ball:
     pass
