cb8512ed2f6bb0a18d9a278d35868c107c2be2a8
jisus <jisus123427@gmail.com>
2024-01-26 21:50:11 +0500

    Создал экран и добавил FPS

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import pygame
 
+pygame.init()
+
+font20 = pygame.font.Font('freesansbold.ttf', 20)
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+
+WIDTH, HEIGHT = 900, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
+
+clock = pygame.time.Clock()
+FPS = 30
+
 
 class Striker:
     pass
@@ -11,6 +26,7 @@ class Ball:
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
